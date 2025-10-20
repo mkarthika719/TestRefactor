9c810e4e663122e2f7c97a47076253251035441e
RAMESH017 <abdramesh007@gmail.com>
2025-10-20 18:49:36 +0530

    Add multiplyWrapper() and update App.java

--- MathUtils.java
+++ MathUtils.java
@@ -2,4 +2,8 @@ public class MathUtils {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
+    public static int multiplyWrapper(int a, int b) {
+        return multiply(a, b);
+    }
 }
